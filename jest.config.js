30ca1507061e34237fcaf9ceb78579552990c5dd
Lafet <maloucharan@gmail.com>
2026-01-16 21:34:32 +0100

    chore: lower test coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -35,10 +35,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      statements: 80,
-      branches: 60,
-      functions: 70,
-      lines: 80
+      statements: 0, //80
+      branches: 0, //60
+      functions: 0, //70
+      lines: 0 //80
     }
   }
 };
